ca00b46d7424adea18b35e401ebf16ed1d1d9037
root <root@DESKTOP-DFB6OFB>
2023-10-05 23:18:13 -0300

    feat: task 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("bacon")
+    assert ingredient1.__hash__() == ingredient2.__hash__()
+    assert ingredient1.__hash__() != ingredient3.__hash__()
+
+    assert ingredient1.__eq__(ingredient2) is True
+    assert ingredient1.__eq__(ingredient1) is True
+    assert ingredient1.__eq__(ingredient3) is False
+
+    assert ingredient1.__repr__() == "Ingredient('queijo mussarela')"
+
+    assert ingredient1.name == "queijo mussarela"
+
+    expected_restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert ingredient1.restrictions == expected_restrictions
